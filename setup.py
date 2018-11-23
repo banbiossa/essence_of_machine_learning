fe649e5c3e84ada29c48c3c2f9702c1ecfcab63b
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-23 18:15:26 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='From the book, Essence of Machine Learning, by Hamukazu Kato',
+    author='Shota Shimizu',
+    license='',
+)
